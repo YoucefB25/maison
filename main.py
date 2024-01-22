5c7191820066ad7e3ee022bbda970e4d998b6c19
Youcef <youcef.boulfrad@ensai.fr>
2024-01-22 22:28:38 +0100

    voilà

---
+++ main.py
@@ -0,0 +1,8 @@
+import datetime
+
+def main():
+    current_time = datetime.datetime.now().strftime("%H:%M:%S")
+    print("L'heure actuelle est :", current_time)
+
+if __name__ == "__main__":
+    main()
